87ae11728329baa24c31f04578c634e6ee5808bd
Efoneli <abbaefoneli@gmail.com>
2023-03-01 23:04:01 +0100

    data

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
 import Creams from './components/Creams';
 import './components/Creams.css'
+import data from './data';
 
 function App() {
+  const newData = data.map(item  => {
+    return (
+      <Creams 
+      img={item.img}
+      name={item.name}
+      contact={item.contact}
+      review={item.review}
+      />
+    )
+  })
   return (
     <div className="main-app">
-      <Creams 
-      img='images/irene.jpg'
-      name='choco delarita'
-      contact='234 544 2345 333'
-      review='yummy!!'
-      />
-       <Creams 
-      img='images/irene.jpg'
-      name='choco delarita'
-      contact='234 544 2345 333'
-      review='yummy!!'
-      />
-       <Creams 
-      img='images/irene.jpg'
-      name='choco delarita'
-      contact='234 544 2345 333'
-      review='yummy!!'
-      />
-       <Creams 
-      img='images/irene.jpg'
-      name='choco delarita'
-      contact='234 544 2345 333'
-      review='yummy!!'
-      />
-       <Creams 
-      img='images/irene.jpg'
-      name='choco delarita'
-      contact='234 544 2345 333'
-      review='yummy!!'
-      />
-       <Creams 
-      img='images/irene.jpg'
-      name='choco delarita'
-      contact='234 544 2345 333'
-      review='yummy!!'
-      />
+      {newData}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,53 @@
+export default [
+    {
+      img:'images/irene.jpg',
+      name:'choco delarita',
+      contact:'234 544 2345 333',
+      review:'yummy!!',
+      id:1,
+    },
+    {
+        img:'images/irene.jpg',
+        name:'choco delarita',
+        contact:'234 544 2345 333',
+        review:'yummy!!',
+        id:2,
+      },
+      {
+        img:'images/irene.jpg',
+        name:'choco delarita',
+        contact:'234 544 2345 333',
+        review:'yummy!!',
+        id:3,
+      },
+      {
+        img:'images/irene.jpg',
+        name:'choco delarita',
+        contact:'234 544 2345 333',
+        review:'yummy!!',
+        id:4,
+      },
+      {
+        img:'images/irene.jpg',
+        name:'choco delarita',
+        contact:'234 544 2345 333',
+        review:'yummy!!',
+        id:5,
+      },
+      {
+        img:'images/irene.jpg',
+        name:'choco delarita',
+        contact:'234 544 2345 333',
+        review:'yummy!!',
+        id:6,
+      },
+      {
+        img:'images/irene.jpg',
+        name:'choco delarita',
+        contact:'234 544 2345 333',
+        review:'yummy!!',
+        id:7,
+      },
+                
+        ]
+    
