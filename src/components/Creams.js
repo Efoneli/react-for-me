3cf2f3385b1a0e7d0e59527db27cb647f2b4afcc
Efoneli <abbaefoneli@gmail.com>
2023-03-02 01:01:11 +0100

    dae

--- src/components/Creams.js
+++ src/components/Creams.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./Creams.css";
 
 const Creams = (props) => {
+  // let badgeText 
+  // if (props.openSpots === 0) {
+  //   badgeText = 'SOLD OUT'
+  // }else if(props.location === 'online') {
+  //   badgeText = 'ONLINE'
+  // }else {
+  //   badgeText = ''
+  // }
   return (
     <div className="card">
       {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
